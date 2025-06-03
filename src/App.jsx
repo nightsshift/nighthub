499a7c50a9dbff3997bca895faba4fb01bf96f6a
NIGHT SHIFT <eyoel@tuta.io>
2025-06-03 17:18:15 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://nighthub-backend.onrender.com');
 
 export default function App() {
   const [message, setMessage] = useState('');
