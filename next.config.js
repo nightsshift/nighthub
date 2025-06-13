2eadb22e573c32c53e5d1210d73ae6a2d49ae835
NIGHT SHIFT <eyoel@tuta.io>
2025-06-13 21:46:21 +0000

    Configure custom domain for nighthub.io

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL: 'https://nighthub.io',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.nighthub.io/:path*',
+      },
+    ];
+  },
 };
+
 module.exports = nextConfig;
